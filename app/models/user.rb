0623dfbd1f12e29889f329d1bb90267863608e8b
amira <amiraabouhadid@gmail.com>
2021-04-19 10:31:59 +0200

    Add post model and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :posts, dependent: :destroy
   validates :username, presence: true, length: { minimum: 8 }
   validates :password, presence: true, length: { minimum: 8 }
   validates :email, presence: true, length: {minimum: 10}
